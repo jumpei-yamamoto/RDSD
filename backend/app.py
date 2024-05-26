cbffd5b0f62dee2b88c4a7d9e0a35c421bc93ddd
jumpei-yamamoto <JumpeiYamamoto@my.uopeople.edu>
2024-05-26 12:06:10 +0900

    port修正

--- backend/app.py
+++ backend/app.py
@@ -59,4 +59,4 @@ def serve(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8002)
+    app.run(debug=True, port=8000)

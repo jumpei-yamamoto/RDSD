2aa991c3ca421230e5bb51258b52a2ce3bec3d8f
jumpei-yamamoto <JumpeiYamamoto@my.uopeople.edu>
2024-05-26 11:12:32 +0900

    frontend/backendの統合

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
@@ -6,7 +6,7 @@ import google.generativeai as gemini
 
 load_dotenv()  # .envファイルから環境変数を読み込む
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 CORS(app, resources={r"/*": {"origins": "*"}})  # CORSを有効にし、すべてのオリジンを許可する
 
 # APIキーを設定
@@ -20,13 +20,10 @@ def invoke_ai():
     features_input = data.get('features')
 
     try:
-        # モデルを準備
         model = gemini.GenerativeModel('gemini-1.5-flash-latest')
         prompt = f'システム名: {system_name}\n特徴: {features_input}\nシステム名に必要な機能を5つ以上記載し、"<>"で囲ってください。'
         response = model.generate_content(prompt)
-        # Extract the generated text from the response
         generated_text = response.candidates[0].content.parts[0].text
-        # Extract features surrounded by <>
         features = [feature.strip('<>') for feature in generated_text.split('**') if feature.startswith('<') and feature.endswith('>')]
         return jsonify({"generated_text": generated_text, "features": features})
     except Exception as e:
@@ -40,13 +37,10 @@ def design_feature():
     feature = data.get('feature')
 
     try:
-        # モデルを準備
         model = gemini.GenerativeModel('gemini-1.5-flash-latest')
         prompt = f'{features_input}の{system_name}に必要な{feature}の設計を箇条書きで教えてください。各機能毎に別カードとして表示したいため、切り分けやすいように返答お願いします。'
         response = model.generate_content(prompt)
-        # Extract the generated design from the response
         design = response.candidates[0].content.parts[0].text
-        # Remove all instances of "**" and format design into bullet points with minimum 5 lines and maximum 10 lines
         cleaned_design = design.replace('**', '')
         design_lines = cleaned_design.split('\n')
         formatted_design = '\n'.join([f'・{line.strip()}' for line in design_lines if line.strip()][:10])
@@ -55,6 +49,14 @@ def design_feature():
         return jsonify({"design": formatted_design})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
